6e90fd648a75f1108424b11784b85f73b65dae67
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-11 15:54:23 +0330

    Update

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
 import sqlite3 as SQL # import library
 
 connection = SQL.connect(r"Files\Database\Password.db") # Connect to the database
+
+cursor = connection.cursor()
+
+# cursor.execute("CREATE TABLE Information (usrnam TEXT, paswrd TEXT)")
+
+cursor.execute("INSERT INTO Information VALUES ('Farbod Parkhooi', 'Farbod@1390')")
+
+rows = cursor.execute("SELECT usrnam, paswrd FROM Information").fetchall()
+print(rows)

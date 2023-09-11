08930de872f8864363a7a193845679e918cedca0
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-11 15:24:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Questions import Questions
 
 def Create_account():
+    USERNAME = input("WRITE YOUR USERNAME: ")
+    PASSWORD = input("WRITE YOUR PASSWORD: ")
     First_Name = input("What's your First name(example: My first name is -Your first name-)? ")
     Last_Name = input("What's your First name(example: My last name is -Your last name-)? ")
     Favorite_animal = input("What's your favorite animal(example: It's -Your favorite animal-)? ")
@@ -8,6 +10,8 @@ def Create_account():
     Favorite_food = input("What's your favorite food(example: It's -Your favorite food-)? ")
 
     Information = Questions(
+        USERNAME=USERNAME
+        PASSWORD=PASSWORD
         First_Name=First_Name, 
         Last_Name=Last_Name, 
         Favorite_animal=Favorite_animal, 

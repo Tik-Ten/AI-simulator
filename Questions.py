75e66131e4a7212fef98368de1634561638ccecf
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-11 14:39:00 +0330

    Add first questions

---
+++ Questions.py
@@ -0,0 +1,7 @@
+def Questions():
+    First_Name = input("What's your First name(example: My first name is -Your first name-)? ")
+    Last_Name = input("What's your First name(example: My last name is -Your last name-)? ")
+    Favorite_animal = input("What's your favorite animal(example: It's -Your favorite animal-)? ")
+    Favorite_fruit = input("What's your favorite fruit(example: It's -Your favorite fruit-)? ")
+    Favorite_food = input("What's your favorite food(example: It's -Your favorite food-)? ")
+    return Favorite_animal + "-" + Favorite_fruit + "-" + First_Name + "-" + Last_Name + "-" + Favorite_food

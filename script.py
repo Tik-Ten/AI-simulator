61ca7d3ab0e139236d399d8266e97307ae49211b
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-11 16:01:40 +0330

    Update

--- script.py
+++ script.py
@@ -1,18 +1,17 @@
-import sqlite3 as SQL # import library
+def Create_account(USERNAME, PASSWORD)
+    import sqlite3 as SQL # import library
 
-connection = SQL.connect(r"Files\Database\Password.db") # Connect to the database
+    connection = SQL.connect(r"Files\Database\Password.db") # Connect to the database
 
-cursor = connection.cursor()
+    cursor = connection.cursor()
 
-# cursor.execute("CREATE TABLE Information (usrnam TEXT, paswrd TEXT)")
+    # cursor.execute("CREATE TABLE Information (usrnam TEXT, paswrd TEXT)")
 
-cursor.execute("INSERT INTO Information VALUES ('Farbod Parkhooi', 'Farbod@1390')")
+    cursor.execute(f"INSERT INTO Information VALUES ('{USERNAME}', '{PASSWORD}')")
 
-Information = cursor.execute("SELECT usrnam, paswrd FROM Information").fetchall()
+    Information = cursor.execute("SELECT usrnam, paswrd FROM Information").fetchall()
 
-Data = {
-    "USERNAME":Information[0][0],
-    "PASSWORD":Information[0][1]
-}
-
-print(Data)
+    Data = {
+        "USERNAME":Information[0][0],
+        "PASSWORD":Information[0][1]
+    }

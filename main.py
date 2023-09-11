a50627fb2af23fe2a62c80f36daff968f65b0dcd
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-09-11 15:20:54 +0330

    Create new def in main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from Questions import Questions
 
-First_Name = input("What's your First name(example: My first name is -Your first name-)? ")
-Last_Name = input("What's your First name(example: My last name is -Your last name-)? ")
-Favorite_animal = input("What's your favorite animal(example: It's -Your favorite animal-)? ")
-Favorite_fruit = input("What's your favorite fruit(example: It's -Your favorite fruit-)? ")
-Favorite_food = input("What's your favorite food(example: It's -Your favorite food-)? ")
+def Create_account():
+    First_Name = input("What's your First name(example: My first name is -Your first name-)? ")
+    Last_Name = input("What's your First name(example: My last name is -Your last name-)? ")
+    Favorite_animal = input("What's your favorite animal(example: It's -Your favorite animal-)? ")
+    Favorite_fruit = input("What's your favorite fruit(example: It's -Your favorite fruit-)? ")
+    Favorite_food = input("What's your favorite food(example: It's -Your favorite food-)? ")
 
-Information = Questions(
-    First_Name=First_Name, 
-    Last_Name=Last_Name, 
-    Favorite_animal=Favorite_animal, 
-    Favorite_fruit=Favorite_fruit,
-    Favorite_food=Favorite_food
-)
+    Information = Questions(
+        First_Name=First_Name, 
+        Last_Name=Last_Name, 
+        Favorite_animal=Favorite_animal, 
+        Favorite_fruit=Favorite_fruit,
+        Favorite_food=Favorite_food
+    )
 
-print(Information.First_Name)

5c224b176fcb504fa79d9bcffd40fad745f8c431
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-12-13 22:01:22 +0330

    Add original main code

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from Questions import Questions
-
-def Create_account():
-    USERNAME = input("WRITE YOUR USERNAME: ")
-    PASSWORD = input("WRITE YOUR PASSWORD: ")
-    First_Name = input("What's your First name(example: My first name is -Your first name-)? ")
-    Last_Name = input("What's your First name(example: My last name is -Your last name-)? ")
-    Favorite_animal = input("What's your favorite animal(example: It's -Your favorite animal-)? ")
-    Favorite_fruit = input("What's your favorite fruit(example: It's -Your favorite fruit-)? ")
-    Favorite_food = input("What's your favorite food(example: It's -Your favorite food-)? ")
-
-    Information = Questions(
-        USERNAME=USERNAME,
-        PASSWORD=PASSWORD,
-        First_Name=First_Name, 
-        Last_Name=Last_Name, 
-        Favorite_animal=Favorite_animal, 
-        Favorite_fruit=Favorite_fruit,
-        Favorite_food=Favorite_food
-    )
-def Login():
-    pass
+from AI_simulator import AI 
+from tkinter import Tk, Label, Button
+Data = AI("Farbod", "Parkhooi", "Cat", "Banana", "Pizza", "swimming", "12", "Female") # Write your information
+root = Tk()
+gender_lable = Label(root, text=f"announcer gender(now:{Data.announcer_gender})", width=25)
+gender_lable.place(x=0, y=0)
+root.title("Farbod AI")
+root.geometry("800x100")
+def Start():
+    root.destroy()
+def Change():
+    gender_lable.destroy()
+    try: genderlable.destroy()
+    except: pass
+    Data.Change_announcer_gender(True)
+    genderlable = Label(root, text=f"announcer gender(now:{Data.announcer_gender})", width=25)
+    genderlable.place(x=0, y=0)
+# Data.Start()
+Button(root, text="Change announcer gender", command=Change).place(x=0, y=25)
+Button(root, text="Who Use Me", command=Data.Who_use_me, font=("", 11), width=15).place(x=250, y=0)
+Button(root, text="Start", command=Start, font=("", 11), width=15).place(x=400, y=0)
+Button(root, text="Test", command=Data.Test, font=("", 11), width=15).place(x=250, y=30)
+Button(root, text="Exit", command=Data.Say_bye, font=("", 11), width=15).place(x=400, y=30)
+root.mainloop()
